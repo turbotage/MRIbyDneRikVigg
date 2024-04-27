075b00fe40d7fd1df53ddc8e81ffe6e8177a7d90
turbotage <viktor.vigren@gmail.com>
2024-04-27 21:19:16 +0200

    le begin

--- viktorplay.py
+++ viktorplay.py
@@ -24,6 +24,9 @@ r2img = ants.image_read(os.path.join(datanat, 'sub-01_R2starmap.nii.gz')).numpy(
 
 # S = PD * sin(alpha) * exp(-TE/T2*) * (1 - exp(-TR/T1)) / (1 - cos(alpha) * exp(-TR/T1))
 
+# S ~ PD * alpha * exp(-TE/T2*)
+# S ~ PD * alpha
+
 # Machine settable
 # alpha
 # TE
@@ -31,23 +34,27 @@ r2img = ants.image_read(os.path.join(datanat, 'sub-01_R2starmap.nii.gz')).numpy(
 
 TR = 0.008
 TE = 0.002
-alpha = 30 * np.pi / 180
+alpha = 15 * np.pi / 180
 
 r2img[r2img < 0] = 0
 
-viewer = napari.Viewer()
-viewer.add_image(1 / r2img, name='T2*')
-viewer.add_image(1 / r1img, name='T1')
-viewer.show(block=True)
+#viewer = napari.Viewer()
+#viewer.add_image(r2img, name='R2*')
+#viewer.add_image(r1img, name='R1')
+#viewer.show(block=True)
 
-E1 = np.exp(-TR * r1img / 1000)
+E1 = np.exp(-TR * r1img)
 E2 = np.exp(-TE * r2img)
-SI = np.sin(alpha) * E2 * (1 - E1) / (1 - np.cos(alpha) * E1)
+SI = pdimg * np.sin(alpha) * E2 * (1 - E1) / (1 - np.cos(alpha) * E1)
+
+lims = np.percentile(SI, [5, 95])
+SI[SI < lims[0]] = lims[0]
+SI[SI > lims[1]] = lims[1]
+
 
 viewer = napari.Viewer()
-viewer.add_image(pdimg, name=f'SI TR={TR} TE={TE} alpha={alpha}')
+viewer.add_image(SI, name=f'SI TR={TR} TE={TE} alpha={alpha}')
 viewer.show(block=True)
-
 
 
 
