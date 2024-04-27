35338586be7f7c7453aa90ec1cce1f77bc793f5c
turbotage <viktor.vigren@gmail.com>
2024-04-27 20:22:40 +0200

    begin

--- viktorplay.py
+++ viktorplay.py
@@ -5,13 +5,50 @@ import os
 
 datanat = os.path.join(os.getcwd(), 'data_anat/')
 
-pdimg = ants.image_read(os.path.join(datanat, 'sub-01_PDmap.nii.gz'))
-r1img = ants.image_read(os.path.join(datanat, 'sub-01_R1map.nii.gz'))
-r2img = ants.image_read(os.path.join(datanat, 'sub-01_R2starmap.nii.gz'))
+pdimg = ants.image_read(os.path.join(datanat, 'sub-01_PDmap.nii.gz')).numpy()
+r1img = ants.image_read(os.path.join(datanat, 'sub-01_R1map.nii.gz')).numpy()
+r2img = ants.image_read(os.path.join(datanat, 'sub-01_R2starmap.nii.gz')).numpy()
+
+#viewer = napari.Viewer()
+#viewer.add_image(pdimg, name='PDmap')
+#viewer.add_image(r1img, name='R1map')
+#viewer.add_image(r2img, name='R2map')
+#viewer.show(block=True)
+
+
+# PD ~ Proton Density = True proton density * camera gain
+# R1 = 1 / T1
+# R2* = 1 / T2*
+
+# Gradient Echo Signal equation
+
+# S = PD * sin(alpha) * exp(-TE/T2*) * (1 - exp(-TR/T1)) / (1 - cos(alpha) * exp(-TR/T1))
+
+# Machine settable
+# alpha
+# TE
+# TR
+
+TR = 0.008
+TE = 0.002
+alpha = 30 * np.pi / 180
+
+r2img[r2img < 0] = 0
 
 viewer = napari.Viewer()
-viewer.add_image(pdimg.numpy(), name='PDmap')
-viewer.add_image(r1img.numpy(), name='R1map')
-viewer.add_image(r2img.numpy(), name='R2map')
+viewer.add_image(1 / r2img, name='T2*')
+viewer.add_image(1 / r1img, name='T1')
 viewer.show(block=True)
+
+E1 = np.exp(-TR * r1img / 1000)
+E2 = np.exp(-TE * r2img)
+SI = np.sin(alpha) * E2 * (1 - E1) / (1 - np.cos(alpha) * E1)
+
+viewer = napari.Viewer()
+viewer.add_image(pdimg, name=f'SI TR={TR} TE={TE} alpha={alpha}')
+viewer.show(block=True)
+
+
+
+
 
